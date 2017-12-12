6a2aad893183a67c4db2d916669ede20ce372b78
mohamed <moh4med.ahmed@gmail.com>
2017-12-12 21:06:19 +0200

    add progress bar for downloading data and error message

--- app/src/main/java/com/example/compucity/mweather/MainActivity.java
+++ app/src/main/java/com/example/compucity/mweather/MainActivity.java
@@ -3,23 +3,30 @@ package com.example.compucity.mweather;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.compucity.mweather.data.SunshinePreferences;
 import com.example.compucity.mweather.utilities.NetworkUtils;
 import com.example.compucity.mweather.utilities.OpenWeatherJsonUtils;
 
-import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tvweatherData;
+    TextView tv_weatherData, tv_errorMessage;
+    ProgressBar pb_weatherloading;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvweatherData = (TextView) findViewById(R.id.tv_weather_data);
+        tv_weatherData = (TextView) findViewById(R.id.tv_weather_data);
+        pb_weatherloading = (ProgressBar) findViewById(R.id.pb_loading_data);
+        tv_errorMessage = (TextView) findViewById(R.id.error_message_tv);
         loadWeatherData();
 
     }
@@ -28,13 +35,41 @@ public class MainActivity extends AppCompatActivity {
         new WeatherDataAsyncTask().execute(SunshinePreferences.getPreferredWeatherLocation(this));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            tv_weatherData.setText("");
+            loadWeatherData();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public class WeatherDataAsyncTask extends AsyncTask<String, Void, String[]> {
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pb_weatherloading.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected void onPostExecute(String[] strings) {
-            if (strings == null) return;
-            for (String s : strings) {
-                tvweatherData.append(s + "\n\n\n");
+            pb_weatherloading.setVisibility(View.INVISIBLE);
+            if (strings == null) {
+                showerrormessage();
+            } else {
+                showweatherdata();
+                for (String s : strings) {
+                    tv_weatherData.append(s + "\n\n\n");
+                }
             }
         }
 
@@ -51,5 +86,15 @@ public class MainActivity extends AppCompatActivity {
             }
             return data;
         }
+    }
+
+    private void showerrormessage() {
+        tv_errorMessage.setVisibility(View.VISIBLE);
+        tv_weatherData.setVisibility(View.INVISIBLE);
+    }
+
+    private void showweatherdata() {
+        tv_errorMessage.setVisibility(View.INVISIBLE);
+        tv_weatherData.setVisibility(View.VISIBLE);
     }
 }
